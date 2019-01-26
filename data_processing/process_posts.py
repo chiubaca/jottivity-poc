dc64d6364685ac87f4fc1a704a8e64f7a731db95
chiubaca <alexchiu11@gmail.com>
2019-01-26 17:30:29 +0000

    git init

---
+++ data_processing/process_posts.py
@@ -0,0 +1,17 @@
+import zipfile
+import os
+
+
+def extractPosts(zipFileName, extractLocation):
+    zip_ref = zipfile.ZipFile( zipFileName, 'r')
+    zip_ref.extractall(extractLocation)
+    zip_ref.close()
+
+extractPosts("all_posts.zip" ,"temp")
+
+#test md file to test out frontmatter lib
+MDFiles = os.listdir("temp")
+print(MDFiles[0])
+
+
+     
