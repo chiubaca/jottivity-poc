2c377781f998eba91a20c44664ab3b149bb63cbd
chiubaca <alexchiu11@gmail.com>
2019-01-26 17:52:49 +0000

    sub git commit

--- data_processing/process_posts.py
+++ data_processing/process_posts.py
@@ -7,10 +7,10 @@ def extractPosts(zipFileName, extractLocation):
     zip_ref.extractall(extractLocation)
     zip_ref.close()
 
-extractPosts("all_posts.zip" ,"temp")
+extractPosts("./all_posts.zip" ,"./temp") 
 
 #test md file to test out frontmatter lib
-MDFiles = os.listdir("temp")
+MDFiles = os.listdir("./temp")
 print(MDFiles[0])
 
 
