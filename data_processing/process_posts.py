a324af383c553f6e5a174feafec7184870c6c8e5
chiubaca <alexchiu11@gmail.com>
2019-02-07 20:26:21 +0000

    basics of python file reading nailed

--- data_processing/process_posts.py
+++ data_processing/process_posts.py
@@ -7,11 +7,30 @@ def extractPosts(zipFileName, extractLocation):
     zip_ref.extractall(extractLocation)
     zip_ref.close()
 
-extractPosts("./all_posts.zip" ,"./temp") 
+# extractPosts("./all_posts.zip" ,"./temp") 
 
-#test md file to test out frontmatter lib
+# an array of all files in the temp folder
 MDFiles = os.listdir("./temp")
-print(MDFiles[0])
 
+print(MDFiles)
 
-     
+#Read a single test post and print
+with open ("temp/" + MDFiles[2], "rt") as testMDFile:
+    
+    lines = testMDFile.readlines()
+    blog_title = lines[0]
+    blog_date = lines[2]
+    blog_mood = lines[3]
+    blog_productivity = lines[4] 
+    blog_contents = lines[6]
+
+# read the title and print 
+    print(blog_title)
+# read the date and print
+    print(blog_date)
+# read the mood and print
+    print(blog_mood)
+# read productivity and print 
+    print(blog_productivity)
+# read contents and print
+    print(blog_contents)
