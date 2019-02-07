4cd50e75abd040ca7281a93c379020190dc6e16b
chiubaca <alexchiu11@gmail.com>
2019-02-07 20:57:31 +0000

    export to json working - need to clean up data further

--- data_processing/process_posts.py
+++ data_processing/process_posts.py
@@ -1,40 +1,44 @@
 import zipfile
 import os
+import json
 
+all_blogs = []
 
 def extractPosts(zipFileName, extractLocation):
     zip_ref = zipfile.ZipFile( zipFileName, 'r')
     zip_ref.extractall(extractLocation)
     zip_ref.close()
 
-# extractPosts("./all_posts.zip" ,"./temp") 
+#extractPosts("./all_posts.zip" ,"./temp") 
 
 # an array of all files in the temp folder
 MDFiles = os.listdir("./temp")
 
 #loop through all files
 for blog in MDFiles:
-    print(blog)
 
-
-    #Read a single test post and print
+    #Read a posts
     with open ("temp/" + blog, "rt") as testMDFile:
-        
+        print("opening " + blog)
+        blog_obj = {}
         lines = testMDFile.readlines()
+        
         blog_title = lines[0]
         blog_date = lines[2]
         blog_mood = lines[3]
         blog_productivity = lines[4] 
         blog_contents = lines[6]
 
-    # read the title and print 
-        print(blog_title)
-    # read the date and print
-        print(blog_date)
-    # read the mood and print
-        print(blog_mood)
-    # read productivity and print 
-        print(blog_productivity)
-    # read contents and print
-        print(blog_contents)
+        blog_obj = {
+            "title": blog_title,
+            "date" : blog_date,
+            "mood" : blog_mood,
+            "productivity" : blog_productivity,
+            "contents" : blog_contents
+                    }
 
+        all_blogs.append(blog_obj)
+
+# print(all_blogs)
+with open("all_blogs.json", "w") as outfile:
+    json.dump(all_blogs, outfile)
