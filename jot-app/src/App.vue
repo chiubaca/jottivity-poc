113ab2a49bec35e264145e9a4f2a56e098513add
chiubaca <alexchiu11@gmail.com>
2019-02-11 20:53:38 +0000

    scafolded the start of the vue app for Jot.

---
+++ jot-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="pen logo" src="./assets/logo.png">
+
+    <AllBlogs />
+  </div>
+</template>
+
+<script>
+import AllBlogs from './components/AllBlogs.vue'
+
+export default {
+  name: 'app',
+  components: {
+    AllBlogs
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ jot-app/src/components/AllBlogs.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+      <h1>The start of Jot.</h1>
+      <h2>An application to jot your thoughts, mood &amp; productivity </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AllBlogs',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
