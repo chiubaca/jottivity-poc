d53bc1c0ad516801fc9437b90a16609a8dcbc85e
Alex Chiu <alexchiu11@gmail.com>
2019-03-15 23:17:49 +0000

    precache json

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,4 +2,23 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-console.log("hello!")
+if(workbox){
+    console.log("workbox is loaded!")
+    workbox.precaching.precacheAndRoute([]);
+    
+    workbox.routing.registerRoute(
+        /(.*)\.(?:json)/,
+        workbox.strategies.networkFirst({
+          cacheName: 'json-cache',
+          plugins: [
+            new workbox.expiration.Plugin({
+              maxEntries: 50,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+            })
+          ]
+        })
+      );
+
+}else{
+    console.log("workbox not loaded")
+}
