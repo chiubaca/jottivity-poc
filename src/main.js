cc0d46706fe317d6ce55d42139ac340c21c74520
Alex Chiu <alexchiu11@gmail.com>
2019-03-15 20:59:20 +0000

    reverted service worker code

--- src/main.js
+++ src/main.js
@@ -6,14 +6,3 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-// Service Worker installation...
-if ('serviceWorker' in navigator){
-  navigator.serviceWorker.register('service-worker.js')
-  .then(function(registration){
-      console.log("registration of SW successful, the scope is: " , registration.scope);
-  })
-  .catch(function(error){
-      console.log("registration of SW failed, error: " , error)
-  })
-}
