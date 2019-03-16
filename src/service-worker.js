95d6582267af0db6b432db177e559ed355e5b5fa
Alex Chiu <alexchiu11@gmail.com>
2019-03-16 23:54:40 +0000

    moved precache manifest out of workbox check

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,9 @@
+self.__precacheManifest = [].concat(self.__precacheManifest || []); //add other resources to cache here
+// workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
 if(workbox){
-    console.log("workbox is loaded!")
-    self.__precacheManifest = [].concat(self.__precacheManifest || []); //add other resources to cache here
-    // workbox.precaching.suppressWarnings();
-    workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-    
+    console.log("workbox is loaded!") 
     workbox.routing.registerRoute(
         /(.*)\.(?:json)/,
         workbox.strategies.networkFirst({
