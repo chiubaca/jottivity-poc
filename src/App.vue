a47b528091dfc5bf5152c9e700eb7ec624c03110
Alex Chiu <alexchiu11@gmail.com>
2019-03-19 05:35:41 +0000

    root dir goes to posts

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <!-- <router-link to="/">Home</router-link> | -->
      
-      <router-link to="/posts">Posts</router-link> |
+      <router-link to="/">Posts</router-link> |
        <router-link to="/about">About</router-link> |
        <router-link to="/login">Login</router-link>
       <header> 
