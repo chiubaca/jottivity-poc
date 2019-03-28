3fb32b4b5ca6fa4d0be59bfea2a1e5b157d0c6c0
Alex Chiu <alexchiu11@gmail.com>
2019-03-28 07:54:45 +0000

    moved fb config into mainjs

--- src/main.js
+++ src/main.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
-import fbConfig from '../firebase.config'
+// import fbConfig from '../firebase.config'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
 let app = "" ;
+
+const fbConfig = {
+  apiKey: "AIzaSyC7h4UCcs1f5uPDNjOW7xVMCJVnm-hWPRg",
+  authDomain: "micro-blog-495b7.firebaseapp.com",
+  databaseURL: "https://micro-blog-495b7.firebaseio.com",
+  projectId: "micro-blog-495b7",
+  storageBucket: "micro-blog-495b7.appspot.com",
+  messagingSenderId: "528040082699"
+};
 
 firebase.initializeApp(fbConfig);
 
