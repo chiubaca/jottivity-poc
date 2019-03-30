312455d1de564b7703e1d43701a57d39c57e2795
Alex Chiu <alexchiu11@gmail.com>
2019-03-30 23:44:21 +0000

    implemented user by user authentication

--- src/pages/AllBlogs.vue
+++ src/pages/AllBlogs.vue
@@ -12,6 +12,8 @@
 import Post from '../components/Post.vue'
 import AddNewPost from '../components/AddNewPost.vue'
 import Logout from '@/components/Logout.vue'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 export default {
   name: "AllBlogs",
@@ -22,26 +24,34 @@ export default {
   },
   data() {
     return {
+      uid:"",
       posts: [],
       tags: []
     };
   },
   methods: {
     getData() {
-      fetch("https://micro-blog-495b7.firebaseio.com/users/alexchiu/notebooks/0.json")
-      .then(response => {
-        return response.json();
-      }).then(myJson => {
+      const uid = firebase.auth().currentUser.uid;
+      
+      firebase.auth().currentUser.getIdToken().then((token) => {
+         const postsURL = `https://micro-blog-495b7.firebaseio.com/users/${uid}/notebooks/0.json`+ `?auth=${token}`
+
+        fetch(postsURL)
+        .then(response => {
+          return response.json();
+        }).then(myJson => {
         //Store all posts from db
-        for (let i in myJson.posts) {
-          this.posts.push(myJson.posts[i]);
-        }
-         //Store all tags from db
-        for (let i in myJson.tags) {
+          for (let i in myJson.posts) {
+            this.posts.push(myJson.posts[i]);
+          }
+          //Store all tags from db
+          for (let i in myJson.tags) {
           this.tags.push(myJson.tags[i]);
-        }
-        // console.log(JSON.stringify(myJson));
-      });
+          }
+          // console.log(JSON.stringify(myJson));
+        });
+      })
+     
     },
  
   },
@@ -50,10 +60,17 @@ export default {
       return true
     }
   },
+  beforeCreate() {
+    //get uid for the session
+    this.uid = firebase.auth().currentUser.uid
+
+  },
   created() {
     this.getData()
 
-  }
+  },
+  
+
 };
 </script>
 
