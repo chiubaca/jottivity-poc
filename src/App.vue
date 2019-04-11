6f43c34c9ce1c8ee98e3ed5839e7a4cdbb11ddc3
Alex Chiu <alexchiu11@gmail.com>
2019-04-11 20:43:51 +0100

    implemented custom header component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/notebooks">Notebooks</router-link> | 
-      <router-link to="/posts">Posts</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link>
-      <header> 
-        <img alt="pen logo" src="./assets/logo.svg">
-          
-        <div>
-          <h1>Jottivity.</h1>
-          <h2>Jot your thoughts, mood &amp; productivity</h2>
-          <h3>PWA Build v0.2.5</h3>
-        </div>
-      </header>
+    <CustomHeader/>
 
-    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import CustomHeader from '@/components/CustomHeader.vue'
+
 export default {
   name: 'app',
   components: {
+    CustomHeader
   }
 }
 </script>
@@ -34,17 +23,6 @@ html *{
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-}
-
-header{
-  display:flex;
-  align-items: center;
-  border-bottom: 5px solid;  
-}
-
-header img{
-  height:100px;
-  padding: 0 10px 0 20px
 }
 
 </style>
