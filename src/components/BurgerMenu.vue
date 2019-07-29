4d1e49d050869e17fab78fabb69ee5b3dfd01b16
Alex Chiu <alexchiu11@gmail.com>
2019-07-29 22:00:44 +0100

    very basic UI for nav slider

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -2,18 +2,33 @@
 
   <div>
     <transition name="fade">
-      <nav v-if="show"
-          :class="show">
-        <button @click="show=!show">hide</button>
-        <router-link to="/notebooks">Notebooks</router-link> | 
-        <router-link to="/about">About</router-link> |
+      <nav v-if="show">
+        <div class="nav-items">
+            <router-link to="/notebooks">📒 Notebooks</router-link>
+            <router-link to="/about">📋 About</router-link>
+        </div>
+
         <Logout/>
+
+        <span class="version-num">🔧 Version 0.4.0 </span>
+
       </nav>
     </transition>
     
-    <button id="hamburger-button"
-            @click="show=!show">🍔
-    </button>
+    <input type="checkbox" id="show-nav">
+    <label for="show-nav">
+      <div id="hamburger-button"
+            @click="show=!show">
+              <svg width="40" height="40" viewBox="0 0 100 100" 
+                   xmlns="http://www.w3.org/2000/svg"
+                   preserveAspectRatio="none">
+                <path class="line line_1" d="M0 40h62c13 0 6 28-4 18L35 35"></path>
+                <path class="line line_2" d="M0 50h70"></path>
+                <path class="line line_3" d="M0 60h62c13 0 6-28-4-18L35 65"></path>
+              </svg>
+      </div>
+    </label>
+    
 
   </div>
 </template>
@@ -33,20 +48,94 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+
+input[type=checkbox]  {
+    display:none;
+}
 
 #hamburger-button{
-  position: absolute;
+  position: fixed;
+  width: 40px;
+  height:40px;
+}
+
+#hamburger-button:hover{
+  background-color: white;
+  border-radius: 0px 0px 10px 0px;
+}
+
+#hamburger-button {
+  background-color: white;
+  border-radius: 0px 0px 10px 0px;
+}
+
+.line {
+  fill: none;
+  stroke:black;
+  stroke-width: 3px;
+  transition: 500ms ease-out;
+  
+  &_1 {
+    stroke-dasharray: 21 100;
+    stroke-dashoffset: 82;
+    transition: 500ms ease-out;
+    
+    
+    input[type=checkbox]:checked + label & {
+      stroke-dashoffset: 26;
+    }
+  }
+  
+  &_2 {
+    stroke-dasharray: 21 100;
+    stroke-dashoffset: 82;
+    transition: 500ms ease-out;
+    // display: none;
+    
+      input[type=checkbox]:checked + label & {
+      stroke-dashoffset: 25;
+    }
+  }
+  
+  &_3 {
+    stroke-dasharray: 21 100;
+    stroke-dashoffset: 82;
+    transition: 500ms ease-out;
+    // display: none;
+    
+      input[type=checkbox]:checked + label & {
+      stroke-dashoffset: 26;
+    }
+  }
 }
 
 nav {
-  z-index: 1;
-  display: flex;
-  flex-direction: column;
-  background: black;
-  width:25vw;
+  margin:0px;
+  padding:0px;
+  border:0px;
+  background: grey;
+  width:270px;
   height:100vh;
   position: fixed;
+}
+
+.nav-items{
+  display:flex;
+  flex-direction: column;
+  padding-top:60px;
+  margin-left:25px;
+}
+
+#logout-button{
+position:absolute;
+  bottom:0;
+  right:0
+}
+
+.version-num{
+  position:absolute;
+  bottom:0;
 }
 
 .fade-enter-active, .fade-leave-active {
