e8c395e4b5c2f4bddcb06b1929f234412674422f
Alex Chiu <alexchiu11@gmail.com>
2019-07-30 05:59:50 +0100

    centered logo removed version number

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -3,34 +3,44 @@
     <header> 
       <img alt="pen logo" src="@/assets/images/logo.svg">
       <div>
-        <h1>Jottivity.</h1>
+        <h1 id="title">      
+          Jottivity.
+        </h1>
         <h2>Jot your thoughts, mood &amp; productivity</h2>
-        <h3>v0.4.0</h3>
       </div>
     </header>
   </div>  
 </template>
 
 <script>
-
-
 export default {
   name: "CustomHeader"
 };
 </script>
 
-
 <style scoped>
+
+#title{
+  font-size:35px
+}
 
 header{
   display:flex;
   align-items: center;
-  border-bottom: 5px solid;  
+  justify-content: center;
+  border-bottom: 5px solid;
+  padding: 10px 0px 10px 0px;  
 }
 
-header img{
-  height:100px;
+img{
+  height:60px;
   padding: 0 10px 0 20px
+}
+
+header div{
+  display: flex;
+  flex-direction: column;
+  align-self: center;
 }
 
 </style>
