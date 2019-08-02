c23fc4957061895c061400d51b99110d1a342135
Alex Chiu <alexchiu11@gmail.com>
2019-08-02 21:49:53 +0100

    global styles  and css tweaks to side bar

--- src/components/BurgerMenu.vue
+++ src/components/BurgerMenu.vue
@@ -4,9 +4,40 @@
     <transition name="slide">
       <nav v-if="show">
         <div class="nav-items">
-            <router-link to="/notebooks">📒 Notebooks</router-link>
-            <router-link to="/about">📋 About</router-link>
-            <span>⚙️ Settings</span>
+
+            <router-link to="/notebooks">
+               <div class="icons">
+                <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="notebookIconTitle">
+                  <title id="notebookIconTitle">Notebook</title>
+                  <path d="M9 8h5"></path>
+                  <path d="M18 3v18H6a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12z"></path>
+                  <path d="M5 19v-1a1 1 0 0 1 1-1h12"></path>
+                </svg>
+                 <span class="black">Notebooks</span>
+              </div>
+            </router-link>
+
+            <router-link to="/about"><div class="icons">
+                <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="stickerIconTitle">
+                  <title id="stickerIconTitle">Sticker</title>
+                  <path d="M21,3 L21,11 C21,16.5228475 16.5228475,21 11,21 L3,21 L3,3 L21,3 Z"></path>
+                  <path d="M9,21 C11.6666667,21 13,19.6666667 13,17 C13,17 13,15.6666667 13,13 L17,13 C19.6666667,13 21,11.6666667 21,9"></path>
+                </svg>
+                <span class="black">About</span>
+              </div>
+            </router-link>
+            
+            <a href="#">
+              <div class="icons">
+                <svg role="img" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" aria-labelledby="settingsIconTitle">
+                  <title id="settingsIconTitle">Settings</title>
+                  <path d="M5.03506429,12.7050339 C5.01187484,12.4731696 5,12.2379716 5,12 C5,11.7620284 5.01187484,11.5268304 5.03506429,11.2949661 L3.20577137,9.23205081 L5.20577137,5.76794919 L7.9069713,6.32070904 C8.28729123,6.0461342 8.69629298,5.80882212 9.12862533,5.61412402 L10,3 L14,3 L14.8713747,5.61412402 C15.303707,5.80882212 15.7127088,6.0461342 16.0930287,6.32070904 L18.7942286,5.76794919 L20.7942286,9.23205081 L18.9649357,11.2949661 C18.9881252,11.5268304 19,11.7620284 19,12 C19,12.2379716 18.9881252,12.4731696 18.9649357,12.7050339 L20.7942286,14.7679492 L18.7942286,18.2320508 L16.0930287,17.679291 C15.7127088,17.9538658 15.303707,18.1911779 14.8713747,18.385876 L14,21 L10,21 L9.12862533,18.385876 C8.69629298,18.1911779 8.28729123,17.9538658 7.9069713,17.679291 L5.20577137,18.2320508 L3.20577137,14.7679492 L5.03506429,12.7050339 Z"></path>
+                  <circle cx="12" cy="12" r="1"></circle>
+                </svg>
+                <span class="black">Settings</span>
+              </div>
+            </a>
+
         </div>
 
         <Logout/>
@@ -20,13 +51,13 @@
     <label for="show-nav">
       <div id="hamburger-button"
             @click="show=!show">
-              <svg width="40" height="40" viewBox="0 0 100 100" 
-                   xmlns="http://www.w3.org/2000/svg"
-                   preserveAspectRatio="none">
-                <path class="line line_1" d="M0 40h62c13 0 6 28-4 18L35 35"></path>
-                <path class="line line_2" d="M0 50h70"></path>
-                <path class="line line_3" d="M0 60h62c13 0 6-28-4-18L35 65"></path>
-              </svg>
+        <svg width="40" height="40" viewBox="0 0 100 100" 
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none">
+          <path class="line line_1" d="M0 40h62c13 0 6 28-4 18L35 35"></path>
+          <path class="line line_2" d="M0 50h70"></path>
+          <path class="line line_3" d="M0 60h62c13 0 6-28-4-18L35 65"></path>
+        </svg>
       </div>
     </label>
     
@@ -109,6 +140,16 @@ input[type=checkbox]  {
   }
 }
 
+.icons svg {
+  width: 30px;
+  height: 30px;
+  stroke: black;
+  stroke-width: 1.3;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: white
+}
+
 nav {
   background: $background-color;
   box-shadow: 0px 2px 5px 1px #7d7d7d;
@@ -128,8 +169,21 @@ nav {
   bottom: 25% 
 }
 
-.nav-items *{
+.nav-items a {
   padding: 20px 20px 20px 35px;
+  text-decoration:none;
+
+    svg {
+      position: relative;
+      top: 10px;
+      right: 5px;
+    }
+
+    span{
+      left: 15px;
+      position: relative;
+    }
+
 }
 
 .nav-items *:hover{
