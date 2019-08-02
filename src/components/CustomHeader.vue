539fc8f9184eb6ee6add7ff931902a868166a863
Alex Chiu <alexchiu11@gmail.com>
2019-08-02 20:07:37 +0100

    Logo and header Tweaks

--- src/components/CustomHeader.vue
+++ src/components/CustomHeader.vue
@@ -2,16 +2,25 @@
   <div>        
     <header> 
       <div class="logo">
+        
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-labelledby="featherIconTitle">
           <title id="featherIconTitle">Feather</title>
           <path d="M5.12634 17C5.04271 17.6571 5 18.325 5 19V21M5.12634 17C6.03384 9.86861 11.7594 4 20 4L19 8H16L17 10L15 12H11L13 14L12 16H8L5.12634 17Z"></path>
         </svg>
+
       </div>
+
       <div>
+
         <h1 id="title">      
           Jottivity.
         </h1>
-        <h2>Jot your thoughts, mood &amp; productivity</h2>
+
+        <h2 id="sub-title">
+          Jot your thoughts,<br/>  
+          mood &amp; productivity
+        </h2>
+      
       </div>
     </header>
   </div>  
@@ -24,7 +33,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 .logo svg {
     width: 90px;
     height: 90px;
@@ -37,6 +45,12 @@ export default {
 
 #title{
   font-size:35px
+}
+
+#sub-title {
+  font-size:11px;
+  text-align: center;
+  letter-spacing: 1.5px;
 }
 
 header{
@@ -57,5 +71,8 @@ header div{
   flex-direction: column;
   align-self: center;
 }
+
+
+
 
 </style>
