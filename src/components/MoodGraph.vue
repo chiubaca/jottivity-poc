458d45c1dcd5e5c91bceb05b523b9d4730d8ea27
Alex Chiu <alexchiu11@gmail.com>
2019-08-05 21:36:29 +0100

    swapped frappe for chartjs

--- src/components/MoodGraph.vue
+++ src/components/MoodGraph.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="mood-graph">
     <div class="blue-loading-spinner" v-if="allPosts.length === 0"></div>
-    <div id="chart"></div>
+    
+      <div id=chart-container>
+        <canvas id="myChart"></canvas>
+      </div>
+    
   </div>
 </template>
 
 <script>
-import { Chart } from "frappe-charts/dist/frappe-charts.esm.js";
-import "frappe-charts/dist/frappe-charts.min.css";
+
+import Charts from 'chart.js';
 
 export default {
   name: "MoodGraph",
@@ -15,25 +19,13 @@ export default {
   mixins: [],
   data() {
     return {
-      chart: {},
-      graphData: {
-        labels: [0,0], //placeholder data, gets overiden in the watcher
-        datasets: [{
-            name: "Mood Score",
-            type: "line",
-            values: [0,0]
-          }],
-        yMarkers: [{
-          label: "Neutral",
-          value:0,
-          options: { labelPos: 'right' } // default: 'right'
-        }]
-      },
+    
     };
   },
   methods: {
   },
   computed:{
+    //Extracts all date object from allPosts and returns an array of dates from all post props
     allDates() {
       let dateRange = [];
       this.allPosts.forEach((post) => {
@@ -56,29 +48,47 @@ export default {
       if (val.length === 0) {
         console.warn("No Data");
         
-      } else if (val.length > 0) {
+      } else if (val.length > 0) {    
 
-        this.chart = new Chart("#chart", {
-          // or a DOM element,
-          // new Chart() in case of ES6 module with above usage
-          data: this.graphData,
-          type: "line", // or 'bar', 'line', 'scatter', 'pie', 'percentage'
-          isNavigable: false, // default: false
-          height: 180,
-          colors: ["#3a8bbb"],
-          axisOptions: {
-            xIsSeries: true // default: false
+        ///CHARTJS
+        let ctx = document.getElementById('myChart');
+        ctx.height = 200;
+        var myChart = new Charts(ctx, {
+          type: 'line',
+          data: {
+            labels: this.allDates,
+            datasets: [{
+              data: this.allSentimentScores,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+              ],
+              borderWidth: 1
+            }]
           },
-          lineOptions: {
-            heatline: 1, // default: 0
-            regionFill: 1, // default: 0
-            hideDots: 1, // default: 0
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+              display: false
+            },
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true,
+                }
+              }],
+              xAxes: [{
+                autoSkip: true,
+                 minRotation: 180,
+              }]
+
+            }
           }
         });
 
-        //Set Data to graph data object
-        this.graphData.datasets[0].values = this.allSentimentScores;
-        this.graphData.labels =  this.allDates;       
       }
     }
   },
@@ -93,5 +103,10 @@ export default {
     display: none;
 }
 
+#chart-container{
+  width:100vh;
+  height:280px;
+  margin: 0 auto;
+}
 
 </style>
